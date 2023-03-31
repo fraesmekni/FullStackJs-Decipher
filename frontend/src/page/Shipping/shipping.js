8b1ad7317226fee8049f5954d78495024d359717
fraesmekni <43705632+fraesmekni@users.noreply.github.com>
2023-03-31 03:34:18 +0100

    fix shipping states

--- frontend/src/page/Shipping/shipping.js
+++ frontend/src/page/Shipping/shipping.js
@@ -8,11 +8,11 @@ import CheckoutSteps from '../../Components/CheckoutSteps/CheckoutSteps'
 const Shipping = () => {
     const navigate = useNavigate();
         const cart = useSelector((state)=>state.cart)
-        const {shippingAddress} = cart
-    const [address, setAddress] = useState(shippingAddress.address)
-    const [city, setCity] = useState(shippingAddress.city)
-    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
-    const [country, setCountry] = useState(shippingAddress.country)
+        const {shippingAddress} = cart || {}
+    const [address, setAddress] = useState(shippingAddress?.address|| '')
+    const [city, setCity] = useState(shippingAddress?.city|| '')
+    const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode|| '')
+    const [country, setCountry] = useState(shippingAddress?.country|| '')
   
     const dispatch = useDispatch()
   
