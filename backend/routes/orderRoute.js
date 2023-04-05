f6e9a9c80369ff8b6abbd8e8f9601f7b2ca35d1a
Eya Amor <eya.amor@esprit.tn>
2023-04-05 23:28:32 +0100

    get All orders of simple user

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router()
-const {addOrderItems,getOrderById,updateOrderToPaid} =require ('../Controllers/orderController.js')
+const {addOrderItems,getOrderById,updateOrderToPaid, getOrders} =require ('../Controllers/orderController.js')
 const { protectSimpleUser,validator,isAdmin }= require('../Middelware/userMiddelware.js')
 
 
 router.post('/',protectSimpleUser,addOrderItems)
 router.get('/:id',protectSimpleUser,getOrderById)
 router.put('/:id/pay',protectSimpleUser,updateOrderToPaid)
+router.get('/getAll/:id',protectSimpleUser,getOrders)
 
 module.exports = router
