d37de2f66ba157d89be817cdbde13ff0d214b12e
Eya Amor <eya.amor@esprit.tn>
2023-04-08 03:34:52 +0100

    approve order backend

--- backend/Controllers/orderController.js
+++ backend/Controllers/orderController.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler")
 const Order = require('../Models/order.js')
 const User = require('../Models/user.js') 
 const Product = require('../Models/product.js') 
+const order = require("../Models/order.js")
 
 
 
@@ -107,6 +108,52 @@ res.status(201).json(userIds);
   });
   
 
+  const OrderApprove = asyncHandler(async (req, res) => {
+    const orderId = req.params.id;
+  
+    const order = await Order.findById(orderId);
+    console.log(order.statusOrder);
+    
+    if (!order) {
+      res.status(404);
+      throw new Error('Order not found');
+    }
+  
+    if (order.statusOrder == false) {
+      order.statusOrder = true;
+      await order.save();
+      res.json("Order Approved");
+      console.log("order is approved");
+    } else {
+      res.status(404);
+      throw new Error('Order not found');
+    }
+  });
+  
+  
+  
+  const OrderNotApprove = asyncHandler(async (req, res) => {
+    const orderId = req.params.id;
+  
+    const order = await Order.findById(orderId);
+    console.log(order.statusOrder);
+    
+    if (!order) {
+      res.status(404);
+      throw new Error('Order not found');
+    }
+  
+    if (order.statusOrder == true) {
+      order.statusOrder = false;
+      await order.save();
+      res.json("Order NOT Approved");
+      console.log("order is NOT approved");
+    } else {
+      res.status(404);
+      throw new Error('Order not found');
+    }
+  });
+  
 
 
 
@@ -180,6 +227,8 @@ const updateOrderToDelivered = asyncHandler(async (req, res) => {
     addOrderItems,getOrderById,updateOrderToPaid,
     getOrders,updateOrderToDelivered,
     getProductUsersIdByOrderId,
-    getProductUsersIdByUserId
-  
+    getProductUsersIdByUserId,
+    OrderApprove
+  ,
+  OrderNotApprove
   }
